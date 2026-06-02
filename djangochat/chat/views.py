7c7f5c28b5646c6417eb2c9013d9b4568a1a439e
dev-lov-oper <ksharvan2007@gmail.com>
2026-06-02 18:36:36 +0530

    Day1

--- djangochat/chat/views.py
+++ djangochat/chat/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def room(request, room):
+    return render(request, 'room.html')
+
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
+
+    if room.objects.filter(name=room).exists():
+        #  query paramater is used to pass data from one view to another view
+         return redirect('/'+room+'/?username='+username)
+    else:
+            new_room = Room.objects.create(name=room)
+            new_room.save()
+        
+
+    return render(request, 'room.html', {'room': room, 'username': username})
